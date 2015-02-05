4454da30a8bba0cb2a269a307eb4b95249eab387
wiraone <xnamine@gmail.com>
2015-02-05 13:36:36 +0700

    Initial Commit

---
+++ Trying/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trying
+//
+//  Created by Owl on 2/5/15.
+//  Copyright (c) 2015 Owl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trying/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trying
+//
+//  Created by Owl on 2/5/15.
+//  Copyright (c) 2015 Owl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
